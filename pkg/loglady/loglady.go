3b2d782a12386e836911eae20b148e7bdcd88dff
Tyler Cunnion <tyler.cunnion@liveramp.com>
2019-08-28 13:23:31 -0700

    Read from external file

--- pkg/loglady/loglady.go
+++ pkg/loglady/loglady.go
@@ -2,34 +2,23 @@ package loglady
 
 import (
 	"io"
+	"io/ioutil"
 
 	"github.com/tylercunnion/loglady/pkg/formatter"
 	"github.com/tylercunnion/loglady/pkg/scanner"
 )
 
-var data = `
-timestamp:
-  parse: "2006-01-02T15:04:05.999Z07:00"
-  format: "Jan _2 15:04:05.0Z"
-levels:
-  error:
-   - err
-fields: 
-  - key: "@timestamp"
-    type: timestamp
-  - key: level
-    format: "[%s]"
-    type: level
-  - key: mdc.application
-    format: "(%s)"
-  - key: message
-`
-
 func Run(r io.Reader) error {
-	var logFmt, err = formatter.GetLineFormat([]byte(data))
+	data, err := ioutil.ReadFile("loglady.yaml")
 	if err != nil {
 		return err
 	}
+
+	logFmt, err := formatter.GetLineFormat(data)
+	if err != nil {
+		return err
+	}
+
 	var scanner = scanner.NewScanner(r, logFmt)
 	err = scanner.Scan()
 	if err != nil {
